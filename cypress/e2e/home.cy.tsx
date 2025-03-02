ada95c8b92b87bfea48be29a533f4023ac4e7f00
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2025-03-02 17:50:45 -0300

    test: criando TODOS dos proximos testes e2e da hoe

--- cypress/e2e/home.cy.tsx
+++ cypress/e2e/home.cy.tsx
@@ -1,11 +1,30 @@
 describe('Home', () => {
-    it('Deve abrir a home', () => {
-        cy.visit('/');
-    });
+    beforeEach(() => cy.visit('/'));
 
-    it('Deve abrir a home a logo', () => {
-        cy.visit('/');
+    describe('TODO', () => {
+        it('Deve renderizar o header', () => {
+            cy.log('Deve renderizar o header');
+            cy.log('Ao realizar a busca do filme deve mostrar a lista de filmes correspondentes a busca');
+            cy.log('Ao clicar no filme listado, deve redirecionar para a pagina do filme clicado');
+        });
 
-        cy.get("[data-testid='header-logo']");
+        it('Deve renderizar os filmes em destaque', () => {
+            cy.log('Deve renderizar a seção filmes em destaque');
+            cy.log('Ao clicar no botão Assitir ao trailer, deve ser redirecionado para a pagina do filme.');
+        });
+
+        it('Deve renderizar os ultimos lançamentos', () => {
+            cy.log('Deve renderizar a seção ultimos lançamentos');
+            cy.log('Ao clicar no botão Assitir ao trailer, deve ser redirecionado para a pagina do filme.');
+        });
+
+        it('Deve renderizar os filmes recomendados', () => {
+            cy.log('Deve renderizar a seção filmes recomendados');
+            cy.log('Ao clicar no botão Assitir ao trailer, deve ser redirecionado para a pagina do filme.');
+        });
+
+        it('Deve renderizar as celebridades', () => {
+            cy.log('Deve renderizar a seção celebridades');
+        });
     });
 });
